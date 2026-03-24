5c1ab09461b07af7d2b0581e71d9538d2663f9fe
Matthew Stark <matthewstark@mac.speedport.ip>
2026-03-24 16:42:38 +0100

    new cpp repo added

---
+++ basics/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Start of hello_world.cpp\n" << endl;
+
+    // TODO: Write your C++ code here
+
+    return 0;
+}
