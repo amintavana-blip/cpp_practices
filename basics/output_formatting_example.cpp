1bd487689b7e390288270587a616605db8929a21
Matthew Stark <matthewstark@155-219.eduroam.rwth-aachen.de>
2026-03-26 14:24:43 +0100

    practice added

---
+++ basics/output_formatting_example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Start of output_formatting_example.cpp\n" << endl;
+
+    double pi = 3.14159265; // Initializing a double variable 'pi' with the value of pi
+
+    return 0;
+}
